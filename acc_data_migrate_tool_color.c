a53651f4ff1d1c3c00e930da89ee170e8f2a6344
Jeff Larkin <jefflarkin@gmail.com>
2022-08-03 17:14:45 -0400

    Comments added.

--- acc_data_migrate_tool_color.c
+++ acc_data_migrate_tool_color.c
@@ -6,6 +6,11 @@
 #define COLOR_BLUE "\033[1;34m" 
 #define COLOR_NC "\033[0m"
 
+/// oaccToolRegister - Register with the OpenACC events API
+///
+/// This function is called by the OpenACC runtime when an event for which it is
+/// registered is issued. It is registerd to particular events with
+/// acc_register_library below.
 void oaccToolRegister(acc_prof_info  *profinfo,
                       acc_event_info *eventinfo,
                       acc_api_info   *apiinfo)
@@ -44,9 +49,17 @@ void oaccToolRegister(acc_prof_info  *profinfo,
     }
   }
 }
+/// acc_register_library - Required registration routine
+///
+/// This routine is specified to be called by the OpenACC runtime
+/// if a profiling library is specified by ACC_PROFLIB. 
 typedef void (*regroutine)( acc_event_t, acc_prof_callback_t, int );
 void acc_register_library(acc_prof_reg regFunc, acc_prof_reg unregFunc, acc_prof_lookup lookup)
 {
+  // regFunc is passed in by the runtime library. This block declared the
+  // events  that our tool cares about and what routine to call if the event is
+  // recorded. Each event does not necessarily need to call the same function,
+  // as done here.
   regFunc(acc_ev_enqueue_upload_start  , oaccToolRegister, 0);
   regFunc(acc_ev_enqueue_upload_end    , oaccToolRegister, 0);
   regFunc(acc_ev_enqueue_download_start, oaccToolRegister, 0);
